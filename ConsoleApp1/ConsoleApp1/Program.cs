56ba20d69d8fe64a660a2740064894c14a2a28e1
som-thing <81899399+som-thing@users.noreply.github.com>
2021-04-04 15:28:32 +0600

    test1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            String source = "12345678987654321";
+            int prevnumber = source[1];
+            for(int i = 1; i <= source.Length; i++) {
+                int currnumber = source[i];
+            }
+
+
+        }
+    }
+}
